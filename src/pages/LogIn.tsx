98307119f5f55596ab6372d716c69356ef26e0d4
savagegraphics <olawaleraji188@gmail.com>
2023-09-12 04:17:41 -0700

    first commit

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,16 +1,5 @@
-import Eight from '@/components/Home/Extended/Featured/Eight'
-import EightX from '@/components/Home/Extended/Featured/EightX'
-import Five from '@/components/Home/Extended/Featured/Five'
-import FiveX from '@/components/Home/Extended/Featured/FiveX'
-import Seven from '@/components/Home/Extended/Media/Seven'
-import SevenX from '@/components/Home/Extended/Media/SevenX'
-import One from '@/components/Home/Extended/Products/One'
-import Three from '@/components/Home/Extended/Products/Three'
-import Six from '@/components/Home/Extended/Showrooms/Six'
-import SixX from '@/components/Home/Extended/Showrooms/SixX'
-import HomePage from '@/components/Home/Extended/Spaces/HomePage'
-import Two from '@/components/Home/Extended/Spaces/Two'
-import Zero from '@/components/Home/Extended/Spaces/Zero'
+import LogIn from '@/components/Home/Extended/LogIn/Login'
+import Footer from '@/components/Home/Extended/Spaces/Footer'
 import React from 'react'
 
 type Props = {}
@@ -18,19 +7,8 @@ type Props = {}
 const Spaces = (props: Props) => {
   return (
     <div>
-      <HomePage/>
-     <Zero/>
-   <One/>
-    <Two/>
-    <Three/>
-    <Five/>
-    <FiveX/>
-    <Six/>
-     <Seven/>
-     <SixX/>
-     <SevenX/>
-     <Eight/>
-     <EightX/>
+      <LogIn/>
+      <Footer/>
     </div>
   )
 }
