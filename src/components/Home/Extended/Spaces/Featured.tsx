a946adcd72d3ccfcf037b78723a116a058e36a28
savagegraphics <olawaleraji188@gmail.com>
2023-09-12 04:23:58 -0700

    first commit

--- src/components/Home/Extended/Spaces/Featured.tsx
+++ src/components/Home/Extended/Spaces/Featured.tsx
@@ -4,44 +4,129 @@ import Image from 'next/image'
 type Props = {}
 
 const Featured = (props: Props) => {
-  return (
-    <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 ml-24 mx-12">
-      <div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
-	<img src="https://source.unsplash.com/random/300x300/?2" alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
-	<div className="flex flex-col justify-between p-6 space-y-8">
-		<div className="space-y-2">
-			<h2 className="text-3xl font-semibold tracki">Donec lectus leo</h2>
-			<p className="dark:text-gray-100">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-		</div>
-		<button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking rounded-md bg-violet-400 text-gray-900">Read more</button>
-	</div>
-</div>
-
-<div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
-	<img src="https://source.unsplash.com/random/300x300/?2" alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
-	<div className="flex flex-col justify-between p-6 space-y-8">
-		<div className="space-y-2">
-			<h2 className="text-3xl font-semibold tracki">Donec lectus leo</h2>
-			<p className="dark:text-gray-100">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-		</div>
-		<button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking rounded-md bg-violet-400 text-gray-900">Read more</button>
-	</div>
-</div>
-
-<div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
-	<img src="https://source.unsplash.com/random/300x300/?2" alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
-	<div className="flex flex-col justify-between p-6 space-y-8">
-		<div className="space-y-2">
-			<h2 className="text-3xl font-semibold tracki">Donec lectus leo</h2>
-			<p className="dark:text-gray-100">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-		</div>
-		<button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking rounded-md bg-violet-400 text-gray-900">Read more</button>
-	</div>
-</div>
-      </div>
-    </div>
-  )
-}
+		return (
+		  <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+			<div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+			  <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+				<img
+				  src="https://images.pexels.com/photos/2408666/pexels-photo-2408666.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+				  className="object-cover w-full h-64"
+				  alt=""
+				/>
+				<div className="p-5 border border-t-0">
+				  <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+					<a
+					  href="/"
+					  className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
+					  aria-label="Category"
+					  title="traveling"
+					>
+					  traveling
+					</a>
+					<span className="text-gray-600">— 28 Dec 2020</span>
+				  </p>
+				  <a
+					href="/"
+					aria-label="Category"
+					title="Visit the East"
+					className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
+				  >
+					Visit the East
+				  </a>
+				  <p className="mb-2 text-gray-700">
+					Sed ut perspiciatis unde omnis iste natus error sit sed quia
+					consequuntur magni voluptatem doloremque.
+				  </p>
+				  <a
+					href="/"
+					aria-label=""
+					className="inline-flex items-center font-semibold transition-colors duration-200 text-purple-400 hover:text-purple-800"
+				  >
+					Learn more
+				  </a>
+				</div>
+			  </div>
+			  <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+				<img
+				  src="https://images.pexels.com/photos/447592/pexels-photo-447592.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+				  className="object-cover w-full h-64"
+				  alt=""
+				/>
+				<div className="p-5 border border-t-0">
+				  <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+					<a
+					  href="/"
+					  className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
+					  aria-label="Category"
+					  title="traveling"
+					>
+					  traveling
+					</a>
+					<span className="text-gray-600">— 28 Dec 2020</span>
+				  </p>
+				  <a
+					href="/"
+					aria-label="Category"
+					title="Simple is better"
+					className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
+				  >
+					Simple is better
+				  </a>
+				  <p className="mb-2 text-gray-700">
+					Sed ut perspiciatis unde omnis iste natus error sit sed quia
+					consequuntur magni voluptatem doloremque.
+				  </p>
+				  <a
+					href="/"
+					aria-label=""
+					className="inline-flex items-center font-semibold transition-colors duration-200 text-purple-400 hover:text-purple-800"
+				  >
+					Learn more
+				  </a>
+				</div>
+			  </div>
+			  <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+				<img
+				  src="https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+				  className="object-cover w-full h-64"
+				  alt=""
+				/>
+				<div className="p-5 border border-t-0">
+				  <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+					<a
+					  href="/"
+					  className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
+					  aria-label="Category"
+					  title="traveling"
+					>
+					  traveling
+					</a>
+					<span className="text-gray-600">— 28 Dec 2020</span>
+				  </p>
+				  <a
+					href="/"
+					aria-label="Category"
+					title="Film It!"
+					className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
+				  >
+					Film It!
+				  </a>
+				  <p className="mb-2 text-gray-700">
+					Sed ut perspiciatis unde omnis iste natus error sit sed quia
+					consequuntur magni voluptatem doloremque.
+				  </p>
+				  <a
+					href="/"
+					aria-label=""
+					className="inline-flex items-center font-semibold transition-colors duration-200 text-purple-400 hover:text-purple-800"
+				  >
+					Learn more
+				  </a>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		);
+	  };
 
 export default Featured
